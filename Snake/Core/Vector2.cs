3d5ee337cab98252ff5c19213b5adb13db0fdc5d
Zantron2000 <xanderpalmer7@gmail.com>
2025-12-08 12:05:32 -0500

    Created Vector2 class

---
+++ Snake/Core/Vector2.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace Snake.Core
+{
+    public struct Vector2(int x, int y)
+    {
+        public int X { get; } = x;
+        public int Y { get; } = y;
+
+        public override readonly bool Equals([NotNullWhen(true)] object? obj)
+        {
+            if (obj is Vector2 other)
+            {
+                return X == other.X && Y == other.Y;
+            }
+            return false;
+        }
+
+        public override readonly int GetHashCode()
+        {
+            return HashCode.Combine(X, Y);
+        }
+    }
+}
