759c406bfe8ba4a59eb546b9db4f4d373ac1cb85
Zantron2000 <xanderpalmer7@gmail.com>
2025-12-18 12:11:30 -0500

    Created the Cell class

---
+++ Snake/Model/Cell.cs
@@ -0,0 +1,35 @@
+using Snake.Core;
+
+namespace Snake.Model
+{
+    public class Cell
+    {
+        private readonly Vector2 position;
+        private IOccupier? occupier;
+
+        public Cell(Vector2 position)
+        {
+            this.position = position;
+        }
+
+        public Vector2 GetPosition()
+        {
+            return position;
+        }
+
+        public IOccupier? GetOccupier()
+        {
+            return occupier;
+        }
+
+        public void SetOccupier(IOccupier? occupier)
+        {
+            this.occupier = occupier;
+        }
+
+        public bool IsOccupied()
+        {
+            return occupier != null;
+        }
+    }
+}
